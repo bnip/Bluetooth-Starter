7f0c1fede948bf5969c49e26dd10bfc381f626b8
Bryan Nip <bryannip@buffalo.edu>
2019-11-14 18:56:39 -0500

    - Added ability to discover Bluetooth Devices and then print the peripherals

--- Bluetooth Starter/ViewController.swift
+++ Bluetooth Starter/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
 
+    var centralManager: CBCentralManager!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-
-
 }
 
+extension ViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+          case .unknown:
+            print("central.state is .unknown")
+          case .resetting:
+            print("central.state is .resetting")
+          case .unsupported:
+            print("central.state is .unsupported")
+          case .unauthorized:
+            print("central.state is .unauthorized")
+          case .poweredOff:
+            print("central.state is .poweredOff")
+          case .poweredOn:
+            centralManager.scanForPeripherals(withServices: nil)
+            print("central.state is .poweredOn")
+        @unknown default:
+            print("Fatal Error")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        print(peripheral)
+    }
+}
